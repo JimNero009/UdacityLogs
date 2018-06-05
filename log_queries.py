c6b6c9da8c382f511a2bb6c6163887c6a50a7e6e
Jim Cockburn <jim.cockburn@skyscanner.net>
2018-06-05 08:55:00 +0100

    formatting

--- log_queries.py
+++ log_queries.py
@@ -3,7 +3,11 @@ from database_query import DatabaseQuery
 
 def most_popular_articles(dbq):
     q = (
-        "select articles.title, reqs.num from (select log.path, count(*) as num from log where log.path like '/article/%' and log.method = 'GET' group by log.path) as reqs, articles where reqs.path like concat('/article/', articles.slug) order by reqs.num desc limit 3;"
+        "select articles.title, reqs.num from "
+        "(select log.path, count(*) as num from log where log.path like '/article/%' "
+        "and log.method = 'GET' group by log.path) as reqs, articles "
+        "where reqs.path like concat('/article/', articles.slug) "
+        "order by reqs.num desc limit 3;"
     )
     result = dbq.perform_query(q)
     print(result)
@@ -11,14 +15,28 @@ def most_popular_articles(dbq):
 
 def most_popular_authors(dbq):
     q = (
-       "select authors.name, views.sum from (select articles.author, sum(reqs.num) as sum from (select log.path, count(*) as num from log where log.path like '/article/%' and log.method = 'GET' group by log.path) as reqs, articles where reqs.path like concat('/article/', articles.slug) group by articles.author) as views, authors where authors.id = views.author order by sum desc;"
+       "select authors.name, views.sum from "
+       "(select articles.author, sum(reqs.num) as sum from "
+       "(select log.path, count(*) as num from log "
+       "where log.path like '/article/%' and log.method = 'GET' group by log.path) "
+       "as reqs, articles where reqs.path like concat('/article/', articles.slug) "
+       "group by articles.author) as views, authors "
+       "where authors.id = views.author order by sum desc;"
     )
     result = dbq.perform_query(q)
     print(result)
 
 def http_error_days(dbq):
     q = (
-        "select total_reqs.date, cast(bad_reqs.total_bad as decimal)/cast(total_reqs.total as decimal) as error_rate from (select date(time) as date, count(log.status) as total from log group by date(log.time) order by date(log.time)) as total_reqs, (select date(time) as date, count(log.status) as total_bad from log where log.status != '200 OK' group by date(log.time) order by date(log.time)) as bad_reqs where total_reqs.date = bad_reqs.date and cast(bad_reqs.total_bad as decimal)/cast(total_reqs.total as decimal) > 0.01"
+        "select total_reqs.date, cast(bad_reqs.total_bad as decimal)/cast(total_reqs.total as decimal) "
+        "as error_rate from (select date(time) as date, "
+        "count(log.status) as total from log group by date(log.time) "
+        "order by date(log.time)) as total_reqs, "
+        "(select date(time) as date, count(log.status) "
+        "as total_bad from log where log.status != '200 OK' "
+        "group by date(log.time) order by date(log.time)) as bad_reqs "
+        "where total_reqs.date = bad_reqs.date and "
+        "cast(bad_reqs.total_bad as decimal)/cast(total_reqs.total as decimal) > 0.01"
     )
     result = dbq.perform_query(q)
     print(result)
