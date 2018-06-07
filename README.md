a818a499346d973cc8239899c8914513af0e6d65
Jim Cockburn <jim.cockburn@skyscanner.net>
2018-06-07 06:26:57 +0100

    address comments

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
-# Database queries
+# Logs Analysis
 
-Simply start the database on your vagrant box, then run log_queries.py as a python program where it can access it.
+## Project Description
+- This projects looks at extracting information from a database using SQL.
+- This script log_queries.py answers three questions useful for real-life situations.
+
+## Requirements
+- Get Virtual Box - https://www.virtualbox.org/wiki/Download_Old_Builds_5_1
+- Get Vagrant - https://www.vagrantup.com/downloads.html
+- Get the vagrantfile - https://s3.amazonaws.com/video.udacity-data.com/topher/2018/April/5acfbfa3_fsnd-virtual-machine/fsnd-virtual-machine.zip
+- Start the vagrant VM by running 'vagrant up' from inside the vagrant sub directory
+- You can now get in to the VM by running 'vagrant ssh'
+
+## Database Set-up
+- Download the files here -  https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f748_newsdata/newsdata.zip
+- Put the file 'newsdata.sql' into the vagrant directory
+- From that directory, run 'psql -d news -f newsdata.sql' to build the database
+
+## How to run
+Execute using a python3 interpreter the log_queries.py script: python log_queries.py
--- log_queries.py
+++ log_queries.py
@@ -1,53 +1,67 @@
+#!/usr/bin/env python3
+
 from database_query import DatabaseQuery
 
 
 def most_popular_articles(dbq):
-    q = (
-        "select articles.title, reqs.num from "
-        "(select log.path, count(*) as num from log where log.path like '/article/%' "
-        "and log.method = 'GET' group by log.path) as reqs, articles "
-        "where reqs.path like concat('/article/', articles.slug) "
-        "order by reqs.num desc limit 3;"
-    )
+    q = """
+        select articles.title, reqs.num from
+        (select log.path, count(*) as num
+        from log where log.path like '/article/%'
+        and log.method = 'GET' group by log.path) as reqs, articles
+        where reqs.path like concat('/article/', articles.slug)
+        order by reqs.num desc limit 3;
+    """
     result = dbq.perform_query(q)
-    print(result)
+    for row in result:
+        print('{} - {} views'.format(row[0], row[1]))
+    print('******************')
 
 
 def most_popular_authors(dbq):
-    q = (
-       "select authors.name, views.sum from "
-       "(select articles.author, sum(reqs.num) as sum from "
-       "(select log.path, count(*) as num from log "
-       "where log.path like '/article/%' and log.method = 'GET' group by log.path) "
-       "as reqs, articles where reqs.path like concat('/article/', articles.slug) "
-       "group by articles.author) as views, authors "
-       "where authors.id = views.author order by sum desc;"
-    )
+    q = """
+       select authors.name, views.sum from
+       (select articles.author, sum(reqs.num) as sum from
+       (select log.path, count(*) as num from log
+       where log.path like '/article/%'
+       and log.method = 'GET' group by log.path)
+       as reqs, articles where
+       reqs.path like concat('/article/', articles.slug)
+       group by articles.author) as views, authors
+       where authors.id = views.author order by sum desc;
+    """
     result = dbq.perform_query(q)
-    print(result)
+    for row in result:
+        print('{} - {} views'.format(row[0], row[1]))
+    print('******************')
+
 
 def http_error_days(dbq):
-    q = (
-        "select total_reqs.date, cast(bad_reqs.total_bad as decimal)/cast(total_reqs.total as decimal) "
-        "as error_rate from (select date(time) as date, "
-        "count(log.status) as total from log group by date(log.time) "
-        "order by date(log.time)) as total_reqs, "
-        "(select date(time) as date, count(log.status) "
-        "as total_bad from log where log.status != '200 OK' "
-        "group by date(log.time) order by date(log.time)) as bad_reqs "
-        "where total_reqs.date = bad_reqs.date and "
-        "cast(bad_reqs.total_bad as decimal)/cast(total_reqs.total as decimal) > 0.01"
-    )
+    q = """
+        select total_reqs.date,
+        cast(bad_reqs.total_bad as decimal)/cast(total_reqs.total as decimal)
+        as error_rate from (select date(time) as date,
+        count(log.status) as total from log group by date(log.time)
+        order by date(log.time)) as total_reqs,
+        (select date(time) as date, count(log.status)
+        as total_bad from log where log.status != '200 OK'
+        group by date(log.time) order by date(log.time)) as bad_reqs
+        where total_reqs.date = bad_reqs.date and
+        cast(bad_reqs.total_bad as decimal)
+        /cast(total_reqs.total as decimal) > 0.01
+    """
     result = dbq.perform_query(q)
-    print(result)
+    for row in result:
+        print('{} - {}%% errors'.format(row[0], int(row[1] * 10000)/100.))
+    print('******************')
 
 
 if __name__ == '__main__':
     dbq = DatabaseQuery()
-    print('Question 1:')
+    print('The most popular three articles of all time:')
     most_popular_articles(dbq)
-    print('Question 2:')
+    print('The most popular article authors of all time:')
     most_popular_authors(dbq)
-    print('Question 3:')
+    print('Days where more than 1%% of requests led to errors:')
     http_error_days(dbq)
     dbq.disconnect()
